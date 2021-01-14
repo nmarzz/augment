538dfb9f27c0ca74062da822a5f20d5389b39d6b
nmarzz <ndmarshall97@gmail.com>
2021-01-13 17:08:07 -0800

    tb tracking

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
+import torchvision
 from torch.autograd import Variable
 import torchnet as tnt
 import seaborn as sns
 import matplotlib.pyplot as plt
 from PIL import Image
+from torch.utils.tensorboard import SummaryWriter
 
 
 # Parser to help call
@@ -25,8 +27,8 @@ parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
                     help='heavy ball momentum in gradient descent (default: 0.9)')
 parser.add_argument('--data-dir', type=str, default='./data',metavar='DIR')
 parser.add_argument('--dataset', type=str, default='FashionMNIST',metavar='DATA')
-parser.add_argument('--visualize', type=bool, default=False,metavar='VIS',
-                    help ='Visualize the data (Default: False)')
+parser.add_argument('--track', type=bool, default=True,metavar='VIS',
+                    help ='Track model training (Default: False)')
 parser.add_argument('--augment', type=bool, default=False,metavar='AUG')
 args = parser.parse_args()
 args.cuda =  torch.cuda.is_available()
@@ -40,7 +42,6 @@ for p in vars(args).items():
 print('\n')
 
 if args.augment:
-
     train_transforms = transforms.Compose([
         transforms.RandomRotation(20),
         transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
@@ -86,6 +87,15 @@ elif args.dataset == 'FashionMNIST':
 
 
 ##############
+def matplotlib_imshow(img, one_channel=False):
+    if one_channel:
+        img = img.mean(dim=0)
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    if one_channel:
+        plt.imshow(npimg, cmap="Greys")
+    else:
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
 #  uuild our models
 class View(nn.Module):
@@ -131,13 +141,20 @@ optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum = args.momentum)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,'min',patience=5,threshold=1e-3,factor = 0.6)
 
 
-if args.visualize:
-    visualization_batch = next(iter(test_loader))
-    vis_batch_data = visualization_batch[0]
+if args.track:
+    writer = SummaryWriter('runs/{}/{}'.format(args.dataset, 'augmented' if args.augment else 'standard'))
+    test_images,y = next(iter(test_loader))
+
+    # Save examples of the train images
+    samples,labels = next(iter(train_loader))
+    img_grid = torchvision.utils.make_grid(samples)
+    matplotlib_imshow(img_grid, one_channel=True)
+    writer.add_image('{}_images'.format(args.dataset), img_grid)
+
     # toPil = transforms.Compose([transforms.ToPILImage(),transforms.Resize((128,128))])
     # toPil(vis_batch_data[0,:,:,:]).show()
-    y = visualization_batch[1].detach().cpu()
-    activations = {'final': [], 'y': y}
+    # y = visualization_batch[1].detach().cpu()
+    # activations = {'final': [], 'y': y}
 
 
 
@@ -155,6 +172,10 @@ def train(epoch):
         loss = loss_function(output, target)
         loss.backward()
         optimizer.step()
+
+        if args.track and batch_ix % 100 == 0 and batch_ix>0:
+            writer.add_scalar('training_loss',loss.data,epoch * len(train_loader) + batch_idx)
+
         if batch_ix % 100 == 0 and batch_ix>0:
             print('[Epoch %2d, batch %3d] training loss: %.4f' %
                 (epoch, batch_ix, loss.data))
@@ -176,6 +197,10 @@ def test():
     print('[Epoch %2d] Average test loss: %.3f, accuracy: %.2f%%\n'
         %(epoch, test_loss.value()[0], top1.value()[0]))
 
+    if args.track:
+        writer.add_scalar('test_loss',test_loss.value()[0],epoch * len(train_loader) + batch_idx)
+        writer.add_scalar('test_acc', top1.value()[0],epoch * len(train_loader) + batch_idx)
+
 
 
 # Actually run
@@ -185,17 +210,17 @@ top1 = tnt.meter.ClassErrorMeter(accuracy = True)
 if __name__=="__main__":
     for epoch in range(1, args.epochs + 1):
         train(epoch)
-        if args.visualize:
-            model.eval()
-            if args.cuda:
-                vis_batch_data = vis_batch_data.cuda()
-            output = model(vis_batch_data)
-            activations['final'].append(output.detach().cpu())
+        # if args.track:
+        #     model.eval()
+        #     if args.cuda:
+        #         vis_batch_data = vis_batch_data.cuda()
+        #     output = model(vis_batch_data)
+        #     activations['final'].append(output.detach().cpu())
         test()
 
-
-    if args.visualize:
-        path = 'activations{}_{}{}.pth'.format(args.dataset,args.epochs, 'augmented' if args.augment else '')
-        activations['loss_meter'] = test_loss
-        activations['top1'] = top1
-        torch.save(activations,path)
+    #
+    # if args.track:
+    #     path = 'activations{}_{}{}.pth'.format(args.dataset,args.epochs, 'augmented' if args.augment else '')
+    #     activations['loss_meter'] = test_loss
+    #     activations['top1'] = top1
+    #     torch.save(activations,path)
